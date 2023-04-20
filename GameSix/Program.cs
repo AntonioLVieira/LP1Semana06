1d8a3d5331dee555d2d4989678134733667dc476
AntonioLVieira <antonio.allvieira@gmail.com>
2023-04-20 16:25:54 +0100

    Ex.5 - pedir número de inimigos + criação de array + pedir o nome para cada inimigo + mostrar o nome de todos os inimigos na tela

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,23 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many enemies?");
+            int number = int.Parse(Console.ReadLine());
+            Foe [] enemies = new Foe [number];
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Enter name");
+                string name = Console.ReadLine();
+                Foe f = new Foe (name);
+                enemies[i] = f;
+            }
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Console.WriteLine(enemies[i].GetName());
+            }
+            
+
         }
     }
 }
+
