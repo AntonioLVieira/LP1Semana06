17f0a73f8a3aa0eed6f1a4fa91378b344da27a43
António Vieira <a22204411@alunos.ulht.pt>
2023-04-21 13:21:15 +0100

    Ex.9 - Criação de nova enumeração chamada Difficulty com 3 níveis de dificuldade + Criação de uma noca classe GameLevel com vários membros (ex.métodos e construtores para as rooms e foes)

---
+++ GameSixFriday/Foe.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace GameSixFriday
+{
+ public class Foe
+ {
+  private int powerUp;
+  private string name;
+  private float health;
+  private float shield;
+  public Foe (string name)
+  {
+   this.name = name;
+   health = 100;
+   shield = 0;
+   powerUp = 0;
+
+
+  }
+  public string GetName()
+  {
+   return name;
+  }
+  public void TakeDamage (float damage)
+  {
+   shield -= damage;
+   if (shield < 0)
+   {
+   float damageStillTolnflict =- shield;
+   shield = 0;
+   health -= damageStillTolnflict;
+   if (health < 0) health = 0;
+   }
+  }
+  public float GetHealth()
+  {
+   return health;
+  }
+  public float GetShield()
+  {
+   return shield;
+  }
+  public void SetName(string newName)
+  {    
+   newName = newName.Trim(); 
+   if (newName.Length > 0 && newName[0] != ' ' && newName[newName.Length - 1] != ' ')
+   {
+    name = newName;
+   }
+  }
+  public int GetPowerUp()
+  {
+   return powerUp;
+  }
+  public void PickupPowerUp(PowerUp p, float f)
+  {
+   if(p == PowerUp.Health)
+   {
+    health += f;
+    powerUp++;
+   }
+   else if(p == PowerUp.Shield)
+   {
+    shield += f;
+    powerUp++;
+   } 
+   else
+   {
+    if (shield > 100 || health > 100)
+    {
+     Console.WriteLine("not possible");
+    }
+
+    
+   }
+  }
+
+ 
+  
+ }
+}
